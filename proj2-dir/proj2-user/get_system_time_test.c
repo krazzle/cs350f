03cc47a47e3ff0d2e1e788c4840f59e5311f6b31
Alyssa Sallean <krazzle@cs.utexas.edu>
2014-03-18 22:05:54 -0500

    fixed stuff

--- proj2-dir/proj2-user/get_system_time_test.c
+++ proj2-dir/proj2-user/get_system_time_test.c
@@ -6,9 +6,9 @@
 
 int main(){
 	struct timeval t;
-	if(syscall(352,&t) < 0)
+	if((n = syscall(352,&t)) < 0)
 	 	printf("Calling get_system_time failed with error code %d\n", n);
 
-	printf("time is (%d,%d)\n", t.tv_sec, t.tv_usec);
+	printf("time is (%d,%d)\n", (int)t.tv_sec, (int)t.tv_usec);
 	return 0;
 }
